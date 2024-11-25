3c5f6dc4790513db10bc394e276f7056fb66fa6f
eehcx.contacto <eehcx.contacto@gmail.com>
2024-11-25 23:23:49 +0000

    minor changes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,18 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import HeaderImg from "@/assets/medium-shot-happy-young-people-partying (1).webp";
-import { NavigationMenuHome } from "@/components/home/navbar";
-
-
+import HeaderImg from "@/assets/happy_people.png";
+//import { NavigationMenuHome } from "@/components/home/navbar";
 
 const Image = LazyLoadImage;
 
 export const Index = () => {
     return (
-        <>
+        <div className="bg-gray-50">
             <header>
-                <nav className="bg-primary">
+                {/*
+                <nav className="bg-gray-200">
                     <NavigationMenuHome />
                 </nav>
+                 */}
 
                 <Image
                     src={HeaderImg}
@@ -22,85 +22,104 @@ export const Index = () => {
                 />
             </header>
 
-            <main>
-                <h1 className="text-5xl font-bold text-primary">
-                    Phrasely
-                </h1>
-                <p>Play and create games for learning english with fun!</p>
-
-
-
-
-                <section className="p-6 rounded-lg w-6/12 mx-auto">
-                    <h2 className="text-3xl font-semibold text-orange-500 mb-4">
-                        Application Instructions
-                    </h2>
-
-                    <h3 className="text-xl font-semibold mt-4">1. Installation</h3>
-                    <p className="text-base mb-4">
-                        Run the installer file and follow the on-screen instructions to
-                        install the application on your PC. After installation, open the
-                        application from the desktop shortcut.
+            <div className="bg-gray-50 py-24 sm:py-32">
+                <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+                    <h2 className="text-center text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
+                    <p className="mx-auto mt-2 max-w-lg text-balance text-center text-4xl font-semibold tracking-tight text-gray-950 sm:text-5xl">
+                    Everything you need to deploy your app
                     </p>
-
-                    <h3 className="text-xl font-semibold mt-4">2. Creating a New Quiz</h3>
-                    <p className="text-base mb-4">
-                        Select "Create Quiz" from the main menu, enter the quiz name, and
-                        choose the topic (e.g., Grammar, Vocabulary, Comprehension). Click
-                        "Add Question" to start adding questions to the quiz.
-                    </p>
-
-                    <h3 className="text-xl font-semibold mt-4">3. Adding Questions</h3>
-                    <p className="text-base mb-4">
-                        Type the question in the designated field and select the question
-                        type:
-                    </p>
-                    <ul className="list-disc list-inside mb-4">
-                        <li>
-                            <strong>Multiple Choice:</strong> Enter answer options and mark
-                            the correct answer.
-                        </li>
-                        <li>
-                            <strong>Open-ended:</strong> Allows the student to type their
-                            response in a text field.
-                        </li>
-                    </ul>
-                    <p className="text-base mb-4">
-                        Repeat this process to add all desired questions, then select "Save
-                        Quiz."
-                    </p>
-
-                    <h3 className="text-xl font-semibold mt-4">
-                        4. Starting a Quiz Session
-                    </h3>
-                    <p className="text-base mb-4">
-                        From the main menu, select "Start Quiz" and choose the quiz you want
-                        to use. The application will display each question on screen, one by
-                        one. Have the student respond, then click "Next" to proceed to the
-                        following question.
-                    </p>
-
-                    <h3 className="text-xl font-semibold mt-4">5. Reviewing Results</h3>
-                    <p className="text-base mb-4">
-                        Upon finishing the quiz, click "Finish" to view a summary of
-                        responses. The app will display correct and incorrect answers,
-                        allowing you to review performance with the student and discuss
-                        areas for improvement.
-                    </p>
-
-                    <h3 className="text-xl font-semibold mt-4">6. Saving Results</h3>
-                    <p className="text-base mb-4">
-                        To save the results, select "Save" and choose a location on your PC
-                        to store the file. This enables you to keep a record of student
-                        performance for future reference.
-                    </p>
-                </section>
-
-
-
-
-            </main>
-
-        </>
+                    <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+                    <div className="relative lg:row-span-2">
+                        <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
+                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+                        <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                            Mobile friendly
+                            </p>
+                            <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                            </p>
+                        </div>
+                        <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
+                            <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
+                            <img
+                                className="size-full object-cover object-top"
+                                src="https://tailwindui.com/plus/img/component-images/bento-03-mobile-friendly.png"
+                                alt=""
+                            />
+                            </div>
+                        </div>
+                        </div>
+                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
+                    </div>
+                    <div className="relative max-lg:row-start-1">
+                        <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
+                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+                        <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Performance</p>
+                            <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.
+                            </p>
+                        </div>
+                        <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                            <img
+                            className="w-full max-lg:max-w-xs"
+                            src="https://tailwindui.com/plus/img/component-images/bento-03-performance.png"
+                            alt=""
+                            />
+                        </div>
+                        </div>
+                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
+                    </div>
+                    <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+                        <div className="absolute inset-px rounded-lg bg-white"></div>
+                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
+                        <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Security</p>
+                            <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                            Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi.
+                            </p>
+                        </div>
+                        <div className="flex flex-1 items-center [container-type:inline-size] max-lg:py-6 lg:pb-2">
+                            <img
+                            className="h-[min(152px,40cqw)] object-cover"
+                            src="https://tailwindui.com/plus/img/component-images/bento-03-security.png"
+                            alt=""
+                            />
+                        </div>
+                        </div>
+                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
+                    </div>
+                    <div className="relative lg:row-span-2">
+                        <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+                        <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                            Powerful APIs
+                            </p>
+                            <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                            Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
+                            </p>
+                        </div>
+                        <div className="relative min-h-[30rem] w-full grow">
+                            <div className="absolute bottom-0 left-10 right-0 top-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
+                            <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                                <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
+                                <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
+                                    NotificationSetting.jsx
+                                </div>
+                                <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
+                                </div>
+                            </div>
+                            <div className="px-6 pb-14 pt-6">{/* Your code example */}</div>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
